ca15951506a0c9add40c8ac705960eeafe208f6f
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-03-30 17:29:01 +0600

    second commit

---
+++ src/components/shop/Shop.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Product from './product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('../../../public/products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+                {
+                    products.map(product => <Product key={product.id} product={product}></Product>)
+                }
+            </div>
+            <div className='cart-container'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
